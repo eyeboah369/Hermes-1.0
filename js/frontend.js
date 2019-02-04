2e23fcf6f7db172412d3ce9765eadc8bbe615e87
eyeboah369 <eyeboahcw@gmail.com>
2019-02-04 10:10:32 -0500

    Update for front-end bindings

--- js/frontend.js
+++ js/frontend.js
@@ -1,4 +1,4 @@
-function fadeOutEffect() {
+/*function fadeOutEffect() {
     var fadeTarget = document.getElementById("main");
     var fadeEffect = setInterval(function () {
         if (!fadeTarget.style.opacity) {
@@ -13,3 +13,31 @@ function fadeOutEffect() {
 }
 
 document.getElementById("main").addEventListener('click', fadeOutEffect);
+*/
+function email() {
+    var input = document.getElementById('userInput').value;
+}
+
+var slideIndex = 1;
+showDivs(slideIndex)
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementById('main');
+    if(n < x.length) {
+        slideIndex  = 1
+    };
+    if (n < 1) {
+        slideIndex = x.length
+    };
+
+    for(i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+    }
+
