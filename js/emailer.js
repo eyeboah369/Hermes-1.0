a3dc4a1036929109cb751e22fbfb3ff8f165788b
eyeboah369 <eyeboahcw@gmail.com>
2019-02-22 13:23:47 -0500

    Changing of window size for functional frontend

--- js/emailer.js
+++ js/emailer.js
@@ -1,8 +1,8 @@
 const nodemailer = require('nodemailer');
 
 let transporter = nodemailer.createTransport ({
-    host: 'eyeboah369@gmail.com';
-    password: "newtonthegreat";
+    host: '';
+    password: '';
 })
 
 /*function()({
